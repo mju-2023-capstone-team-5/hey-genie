b16cefc5c742a1c04772bc4afcbd44d8d66fcfa3
915dbfl <dbfl00915@naver.com>
2023-04-10 01:57:17 +0900

    [Feat] #4 mainActivity에 fragemtnNacContainer view 추가 및 navController 처리

--- app/src/main/java/org/sopar/presentation/main/MainActivity.kt
+++ app/src/main/java/org/sopar/presentation/main/MainActivity.kt
@@ -2,11 +2,29 @@ package org.sopar.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import org.sopar.R
+import org.sopar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setJetpackNavigation()
+
+    }
+
+    private fun setJetpackNavigation() {
+
+        val host = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = host.navController
+
     }
 }
