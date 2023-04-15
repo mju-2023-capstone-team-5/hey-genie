8a04688a7f1e45ec97014376cbe47a3fdbc538d3
915dbfl <dbfl00915@naver.com>
2023-04-15 21:46:12 +0900

    [Fix] #3 kakao sdk init 과정에서 오류 수정

--- app/src/main/java/org/sopar/presentation/base/GlobalApplication.kt
+++ app/src/main/java/org/sopar/presentation/base/GlobalApplication.kt
@@ -2,11 +2,12 @@ package org.sopar.presentation.base
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import org.sopar.BuildConfig
 
 class GlobalApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
-        KakaoSdk.init(this, "{NATIVE_APP_KEY")
+        KakaoSdk.init(this, BuildConfig.NATIVE_APP_KEY)
     }
 }
