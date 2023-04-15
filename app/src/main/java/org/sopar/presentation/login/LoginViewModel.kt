0d593de8ad7598cbd2eb1e3b1629b81e49fd51ff
915dbfl <dbfl00915@naver.com>
2023-04-15 22:34:13 +0900

    [Feat] #3 로그인 토큰 존재 여부 처리

--- app/src/main/java/org/sopar/presentation/login/LoginViewModel.kt
+++ app/src/main/java/org/sopar/presentation/login/LoginViewModel.kt
@@ -2,16 +2,24 @@ package org.sopar.presentation.login
 
 import android.content.Context
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.kakao.sdk.auth.AuthApiClient
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.common.model.KakaoSdkError
 import com.kakao.sdk.user.UserApiClient
 
 class LoginViewModel: ViewModel() {
+    private val _error = MutableLiveData(false)
+    val error: LiveData<Boolean> get() = _error
+
     private val callback: (OAuthToken?, Throwable?) -> Unit = { oAuthToken, throwable ->
         if (throwable != null) {
             Log.e("error", "카카오계정으로 로그인 실패", throwable)
+            _error.postValue(true)
         } else if (oAuthToken != null) {
             Log.i("success", "카카오계정으로 로그인 성공 ${oAuthToken.accessToken}")
         }
@@ -30,6 +38,20 @@ class LoginViewModel: ViewModel() {
                 //서버에 유저 확인
             } else {
                 UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+            }
+        }
+    }
+
+    suspend fun checkToken() {
+        if (AuthApiClient.instance.hasToken()) {
+            UserApiClient.instance.accessTokenInfo { _, error ->
+                if (error != null) {
+                    if ((error !is KakaoSdkError) || !error.isInvalidTokenError()) {
+                        _error.postValue(true)
+                    }
+                } else {
+                    //로그인 처리 진행
+                }
             }
         }
     }
