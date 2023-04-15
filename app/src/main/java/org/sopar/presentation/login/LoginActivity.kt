089c3f23fe5ad1148ad3e7a6b3abf4e18ba18a20
915dbfl <dbfl00915@naver.com>
2023-04-15 21:50:16 +0900

    [Feat] #3 카카오 로그인 구현 완료

--- app/src/main/java/org/sopar/presentation/login/LoginActivity.kt
+++ app/src/main/java/org/sopar/presentation/login/LoginActivity.kt
@@ -1,16 +1,31 @@
 package org.sopar.presentation.login
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.sopar.databinding.ActivityLoginBinding
 
 class LoginActivity: AppCompatActivity() {
     private val binding: ActivityLoginBinding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
     }
+    private val loginViewModel by viewModels<LoginViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        binding.buttonKakaoLogin.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                loginViewModel.loginWithKakao(this@LoginActivity)
+            }
+        }
     }
 }
