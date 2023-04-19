bd8dd78cca111eda71e08125bc1897205d0c8032
915dbfl <dbfl00915@naver.com>
2023-04-19 19:52:49 +0900

    [Feat] #13 로그인 관련 api 정의

--- app/src/main/java/org/sopar/data/api/RetrofitApi.kt
+++ app/src/main/java/org/sopar/data/api/RetrofitApi.kt
@@ -1,4 +1,16 @@
 package org.sopar.data.api
 
+import org.sopar.data.remote.request.LoginRequest
+import org.sopar.data.remote.response.LoginResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface RetrofitApi {
+
+    @POST("/api/v1/login/token")
+    suspend fun login(
+        @Body loginRequest: LoginRequest
+    ): Response<LoginResponse>
+
 }
