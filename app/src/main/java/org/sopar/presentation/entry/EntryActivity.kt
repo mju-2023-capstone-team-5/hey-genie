911ddd833c9e1de51e297f61c0a67ce4cbb09d18
915dbfl <dbfl00915@naver.com>
2023-04-21 16:14:38 +0900

    [Feat] #26 시간/월주차에 따라 화면 전환시 넘겨주는 값 구분 처리

--- app/src/main/java/org/sopar/presentation/entry/EntryActivity.kt
+++ app/src/main/java/org/sopar/presentation/entry/EntryActivity.kt
@@ -1,4 +1,41 @@
 package org.sopar.presentation.entry
 
-class EntryActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.sopar.databinding.ActivityEntryBinding
+import org.sopar.presentation.main.MainActivity
+
+class EntryActivity: AppCompatActivity() {
+    private val binding: ActivityEntryBinding by lazy {
+        ActivityEntryBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        binding.btnRegister.setOnClickListener {
+            //등록하기 화면으로 전환
+        }
+
+        binding.btnHourlyParking.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("isHourly", true)
+            startActivity(intent)
+        }
+
+        binding.btnMonthlyParking.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("isHourly", false)
+            startActivity(intent)
+        }
+
+        binding.btnNotice.setOnClickListener {
+            //공지사항 화면 구성
+        }
+    }
 }
