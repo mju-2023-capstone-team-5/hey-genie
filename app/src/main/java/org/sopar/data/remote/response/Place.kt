8c5794f24ceacfd7fa749a5ab09ebb2cdf688320
915dbfl <dbfl00915@naver.com>
2023-05-03 16:23:33 +0900

    [Feat] #25 kakao 주소 검색 api response 모델 정의

---
+++ app/src/main/java/org/sopar/data/remote/response/Place.kt
@@ -0,0 +1,12 @@
+package org.sopar.data.remote.response
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Place(
+    @field:Json(name = "place_name")
+    val place_name: String,
+    @field:Json(name = "road_address_name")
+    val road_address_name: String
+)
