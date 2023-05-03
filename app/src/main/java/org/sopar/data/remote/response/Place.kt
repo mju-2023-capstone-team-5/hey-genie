ff1e93e7a32648c2c1e5c46c5f9090afc6ac8237
915dbfl <dbfl00915@naver.com>
2023-05-03 17:18:05 +0900

    [Fix] #25 주소 검색 응답 data model 구조 변경

--- app/src/main/java/org/sopar/data/remote/response/Place.kt
+++ app/src/main/java/org/sopar/data/remote/response/Place.kt
@@ -8,5 +8,9 @@ data class Place(
     @field:Json(name = "place_name")
     val place_name: String,
     @field:Json(name = "road_address_name")
-    val road_address_name: String
+    val road_address_name: String,
+    @field:Json(name = "x")
+    val x: String,
+    @field:Json(name = "y")
+    val y: String
 )
