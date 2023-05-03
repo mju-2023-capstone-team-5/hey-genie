460663b6c2f7718c8811d960b116287c5e9b1c47
915dbfl <dbfl00915@naver.com>
2023-05-03 16:40:36 +0900

    [Feat] #25 주소 검색 api 정의

--- app/src/main/java/org/sopar/data/api/RetrofitApi.kt
+++ app/src/main/java/org/sopar/data/api/RetrofitApi.kt
@@ -3,9 +3,13 @@ package org.sopar.data.api
 import org.sopar.data.remote.request.LoginRequest
 import org.sopar.data.remote.request.UserRegisterRequest
 import org.sopar.data.remote.response.LoginResponse
+import org.sopar.data.remote.response.SearchResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface RetrofitApi {
 
@@ -18,5 +22,11 @@ interface RetrofitApi {
     suspend fun userRegister(
         @Body userRegisterRequest: UserRegisterRequest
     ): Response<String>
+
+    @GET("v2/local/search/keyword.json")
+    suspend fun getSearchLocation(
+        @Header("Authorization") token: String,
+        @Query("query") query: String
+    ): Response<SearchResponse>
 
 }
