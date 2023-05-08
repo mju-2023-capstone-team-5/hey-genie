f639aecfb11f01020c4636ad428f9fcf92baac7f
915dbfl <dbfl00915@naver.com>
2023-05-08 15:23:59 +0900

    [Feat] #34 주차장 정보 recyclerview에 사용할 viewholder, adapter 정의

---
+++ app/src/main/java/org/sopar/presentation/map/ParkingLotViewHolder.kt
@@ -0,0 +1,21 @@
+package org.sopar.presentation.map
+
+import com.bumptech.glide.Glide
+import org.sopar.data.remote.response.ParkingLot
+import org.sopar.databinding.ItemParkingLotBinding
+import org.sopar.presentation.base.BaseViewHolder
+
+class ParkingLotViewHolder(
+    private val binding: ItemParkingLotBinding
+): BaseViewHolder<ParkingLot>(binding.root) {
+
+    override fun bind(item: ParkingLot) {
+        binding.textName.text = item.name
+        binding.textAddress.text = item.address
+        binding.textRemaining.text = item.remainingSpace.toString()
+        item.imageUrl?.let {
+            Glide.with(binding.root).load(item.imageUrl).into(binding.imageParkingLot)
+        }
+    }
+
+}
