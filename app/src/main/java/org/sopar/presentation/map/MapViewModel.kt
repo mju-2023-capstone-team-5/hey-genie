1f290f926f6fc64f45cec0b03eb6fc90e57523a9
915dbfl <dbfl00915@naver.com>
2023-05-15 12:58:32 +0900

    [Feat] #44 화면 영역에 해당되는 주차장 피커 표시 및 recyclerview에 출력

--- app/src/main/java/org/sopar/presentation/map/MapViewModel.kt
+++ app/src/main/java/org/sopar/presentation/map/MapViewModel.kt
@@ -1,5 +1,6 @@
 package org.sopar.presentation.map
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,6 +8,7 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import net.daum.mf.map.api.MapPoint
 import org.sopar.data.remote.response.ParkingLot
 import org.sopar.domain.entity.NetworkState
 import org.sopar.domain.repository.MapRepository
@@ -21,20 +23,24 @@ class MapViewModel @Inject constructor(
     val getParkingLotState: LiveData<NetworkState> = _getParkingLotState
     private val _parkingLots = MutableLiveData<List<ParkingLot>>(listOf())
     val parkingLots: LiveData<List<ParkingLot>> = _parkingLots
+    private val _mapCenter = MutableLiveData(MapPoint.mapPointWithGeoCoord(-1.0E7, -1.0E7))
+    val mapCenter: LiveData<MapPoint> = _mapCenter
 
-    fun getParkingLots(x1: Double, y1: Double, x2: Double, y2: Double){
+    fun getParkingLots(x1: Double, x2: Double, y1: Double, y2: Double){
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                //목 데이터
-                val response = mapRepository.getParkingLots(0.0, 0.0, 0.0, 0.0)
-                //실제 사용할 부분
-                //val response = mapRepository.getParkingLots(x1, y1, x2, y2)
+                val response = mapRepository.getParkingLots(x1, y1, x2, y2)
+                Log.e("map viewmodel", response.body().toString())
                 _parkingLots.postValue(response.body())
                 _getParkingLotState.postValue(NetworkState.SUCCESS)
             } catch (e: IOException) {
                 _getParkingLotState.postValue(NetworkState.FAIL)
             }
         }
+    }
+
+    fun updateMapCenter(mapPoint: MapPoint?) {
+        _mapCenter.postValue(mapPoint)
     }
 
 }
