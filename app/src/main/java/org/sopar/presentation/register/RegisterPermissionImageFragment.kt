33525675d4e5e050938a9c9cc378c4c3bb7acb42
915dbfl <dbfl00915@naver.com>
2023-05-24 03:23:59 +0900

    [Feat] #43 주차장 등록 로직 구현

--- app/src/main/java/org/sopar/presentation/register/RegisterPermissionImageFragment.kt
+++ app/src/main/java/org/sopar/presentation/register/RegisterPermissionImageFragment.kt
@@ -1,5 +1,6 @@
 package org.sopar.presentation.register
 
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,14 +8,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.net.toUri
 import androidx.fragment.app.activityViewModels
 import androidx.viewpager2.widget.ViewPager2
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopar.R
+import org.sopar.data.remote.request.ParkingLotRequest
 import org.sopar.databinding.FragmentRegisterPermissionImageBinding
+import org.sopar.domain.entity.NetworkState
 import org.sopar.presentation.base.BaseErrorDialog
 import org.sopar.presentation.base.BaseFragment
 import org.sopar.util.Constants
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 @AndroidEntryPoint
 class RegisterPermissionImageFragment: BaseFragment<FragmentRegisterPermissionImageBinding>(R.layout.fragment_register_permission_image) {
@@ -30,12 +37,52 @@ class RegisterPermissionImageFragment: BaseFragment<FragmentRegisterPermissionIm
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setListener()
+        setObserve()
+    }
+
+    private fun setObserve() {
+        viewModel.registerStatus.observe(viewLifecycleOwner) { state ->
+            if (state == NetworkState.SUCCESS) {
+                registerImage()
+            } else {
+                val dialog = BaseErrorDialog(R.string.base_error)
+                dialog.show(requireActivity().supportFragmentManager, "RegisterErrorDialog")
+            }
+
+        }
+    }
+
+    private fun registerImage() {
+        val parkingLotFile = File(getRealPath(viewModel.imageUrl!!.toUri()))
+        val permissionFile = File(getRealPath(viewModel.permissionUrl!!.toUri()))
+        viewModel.registerParkingLotImage(parkingLotFile)
+        viewModel.registerPermissionImage(permissionFile)
+    }
+
+    private fun getRealPath(uri: Uri): String?{
+        val contentResolver = requireContext().contentResolver
+        val filePath = requireContext().applicationInfo.dataDir + File.separator + System.currentTimeMillis() + ".jpg"
+        val file = File(filePath)
+
+        try{
+            val inputStream = contentResolver.openInputStream(uri) ?: return null
+            val outputStream = FileOutputStream(file)
+            val buf = ByteArray(1024)
+            var len: Int
+            while(inputStream.read(buf).also { len = it } > 0)
+                outputStream.write(buf, 0, len)
+            outputStream.close()
+            inputStream.close()
+        }catch(ignore: IOException){
+            return null
+        }
+        return file.absolutePath
     }
 
     private fun setListener() {
 
         binding.imageGallery.setOnClickListener {
-            if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.Q) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                 requestGalleryLauncher.launch(Constants.REQUIRED_GALLERY_PERMISSION)
             } else {
                 requestGalleryLauncher.launch(Constants.REQUIRED_EXTERNAL_STORAGE)
@@ -43,7 +90,7 @@ class RegisterPermissionImageFragment: BaseFragment<FragmentRegisterPermissionIm
         }
 
         binding.imageDocument.setOnClickListener {
-            if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.Q) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                 requestGalleryLauncher.launch(Constants.REQUIRED_GALLERY_PERMISSION)
             } else {
                 requestGalleryLauncher.launch(Constants.REQUIRED_EXTERNAL_STORAGE)
@@ -56,27 +103,72 @@ class RegisterPermissionImageFragment: BaseFragment<FragmentRegisterPermissionIm
             } else {
                 val viewPager = requireActivity().findViewById<ViewPager2>(R.id.register_view_pager)
                 viewPager.setCurrentItem(8, true)
+
+                registerParkingLot()
+
+                Toast.makeText(context, "등록이 완료되었습니다!", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    private val requestGalleryLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()){ isGranted->
-        if (isGranted) {
-            getGalleryLauncher.launch("image/*")
-        } else {
-            val dialog = BaseErrorDialog(R.string.permission_error)
-            dialog.show(requireActivity().supportFragmentManager, "reques")
-        }
+
+    private fun registerParkingLot() {
+        val name = viewModel.name!!
+        val address = viewModel.address!!
+        val freeInformation = viewModel.freeInformation
+        val phoneNumber = viewModel.phoneNumber
+        val latitude = viewModel.latitude
+        val longitude = viewModel.longitude
+        val totalSpace = viewModel.totalSpace
+        val remainingSpace = viewModel.totalSpace
+        val ownerId = viewModel.ownerId
+        val type = viewModel.type
+        val availableDay = viewModel.availableDay
+        val availableTime = viewModel.availableTime
+        val monthly = viewModel.monthly
+        val hourly = viewModel.hourly
+
+        val parkingLot = ParkingLotRequest(
+            name = name,
+            address = address,
+            freeInformation = freeInformation,
+            phoneNumber = phoneNumber!!,
+            latitude = latitude!!,
+            longitude = longitude!!,
+            totalSpace = totalSpace!!,
+            remainingSpace = remainingSpace!!,
+            ownerId = null,
+            type = type!!,
+            availableDay = availableDay!!,
+            availableTime = availableTime!!,
+            monthly = monthly,
+            hourly = hourly,
+        )
+
+        viewModel.registerParkingLot(parkingLot)
     }
 
-    private val getGalleryLauncher = registerForActivityResult(
-        ActivityResultContracts.GetContent()) { uri ->
-        uri?.let {
-            viewModel.permissionUrl = it.toString()
-            binding.imageDocument.visibility = View.VISIBLE
-            binding.imageDocument.setImageURI(it)
-            binding.imageGallery.visibility = View.GONE
+
+        private val requestGalleryLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                getGalleryLauncher.launch("image/*")
+            } else {
+                val dialog = BaseErrorDialog(R.string.permission_error)
+                dialog.show(requireActivity().supportFragmentManager, "reques")
+            }
         }
-    }
+
+        private val getGalleryLauncher = registerForActivityResult(
+            ActivityResultContracts.GetContent()
+        ) { uri ->
+            uri?.let {
+                viewModel.permissionUrl = it.toString()
+                binding.imageDocument.visibility = View.VISIBLE
+                binding.imageDocument.setImageURI(it)
+                binding.imageGallery.visibility = View.GONE
+            }
+        }
+
 }
--- app/src/main/java/org/sopar/presentation/register/RegisterViewModel.kt
+++ app/src/main/java/org/sopar/presentation/register/RegisterViewModel.kt
@@ -7,8 +7,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
 import org.sopar.BuildConfig
+import org.sopar.data.remote.request.ParkingLotRequest
+import org.sopar.data.remote.response.ParkingLot
 import org.sopar.data.remote.response.Place
 import org.sopar.data.remote.response.Rate
 import org.sopar.data.remote.response.TimeSlot
@@ -16,7 +23,10 @@ import org.sopar.domain.entity.NetworkState
 import org.sopar.domain.repository.AuthRepository
 import org.sopar.domain.repository.KakaoRepository
 import org.sopar.domain.repository.ParkingLotRepository
+import retrofit2.Response
+import java.io.File
 import java.lang.Exception
+import java.time.LocalDate
 import javax.inject.Inject
 
 @HiltViewModel
@@ -32,10 +42,7 @@ class RegisterViewModel @Inject constructor(
     var latitude: Double? = null
     var longitude: Double? = null
     var totalSpace: Int? = null
-    val remainingSpace: Int? = null
     val ownerId: Int? = null
-    val minimum: Int? = null
-    val surcharge: Int? = null
     var type: List<String>? = null
     var availableDay: List<String>? = null
     var availableTime: List<TimeSlot>? = null
@@ -48,6 +55,12 @@ class RegisterViewModel @Inject constructor(
     private val _searchResult = MutableLiveData<List<Place>>(listOf())
     val searchResult: LiveData<List<Place>> get() = _searchResult
 
+    private val _registerStatus = MutableLiveData(NetworkState.LOADING)
+    val registerStatus: LiveData<NetworkState> get() = _registerStatus
+
+    private var _parkingLotId: Int? = null
+    private val parkingLotId: Int get() = _parkingLotId!!
+
     fun getSearchLocation(query: String) {
         viewModelScope.launch(Dispatchers.IO){
             try {
@@ -58,4 +71,73 @@ class RegisterViewModel @Inject constructor(
             }
         }
     }
+
+    fun registerParkingLot(parkingLotRequest: ParkingLotRequest) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val userId = authRepository.getUId().first()
+                parkingLotRequest.ownerId = userId
+                Log.d("parkingLot", parkingLotRequest.toString())
+                val response: Response<ParkingLot> = parkingLotRepository.registerParkingLot(parkingLotRequest)
+                if (response.isSuccessful) {
+                    Log.d("register parkingLot result", response.body().toString())
+                    _parkingLotId = response.body()!!.id
+                    _registerStatus.postValue(NetworkState.SUCCESS)
+                } else {
+                    Log.d("register parkingLot result", response.code().toString())
+                    _registerStatus.postValue(NetworkState.FAIL)
+                }
+
+            } catch (e: Exception) {
+                Log.d("register parkingLot error", e.toString())
+                _registerStatus.postValue(NetworkState.FAIL)
+            }
+        }
+    }
+
+    fun registerParkingLotImage(image: File) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                var imageRequestBody: RequestBody?
+                var imageMultipartBody: MultipartBody.Part? = null
+                image?.let {
+                    imageRequestBody = image.asRequestBody("image/jpeg".toMediaType())
+                    imageMultipartBody = MultipartBody.Part.createFormData(name= "file", filename = image.name, body = imageRequestBody!!)
+                }
+
+                val response = parkingLotRepository.registerParkingLotImage(parkingLotId, imageMultipartBody!!)
+                Log.d("register parkinglot image", response.code().toString())
+                if (! response.isSuccessful) {
+                    _registerStatus.postValue(NetworkState.FAIL)
+                }
+
+            } catch (e: Exception) {
+                Log.d("register parkingLot image", e.toString())
+            }
+        }
+    }
+
+    fun registerPermissionImage(image: File) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                var imageRequestBody: RequestBody?
+                var imageMultipartBody: MultipartBody.Part? = null
+                image?.let {
+                    imageRequestBody = image.asRequestBody("image/jpeg".toMediaType())
+                    imageMultipartBody = MultipartBody.Part.createFormData(name= "file", filename = image.name, body = imageRequestBody!!)
+                }
+
+                val response = parkingLotRepository.registerPermissionImage(parkingLotId, listOf(imageMultipartBody!!))
+                if (! response.isSuccessful) {
+                    Log.d("register permission image", response.code().toString())
+                    _registerStatus.postValue(NetworkState.FAIL)
+                }
+
+            } catch (e: Exception) {
+                Log.d("register permission image", e.toString())
+            }
+        }
+    }
+
+
 }
