f10d8007cee3a8451039584041c0f0dfbca8a2f5
915dbfl <dbfl00915@naver.com>
2023-05-25 18:06:34 +0900

    [Feat] #49 주차 관리 로직 처리

--- app/src/main/java/org/sopar/presentation/myParkingLot/MyParkingLot.kt
+++ app/src/main/java/org/sopar/presentation/myParkingLot/MyParkingLot.kt
@@ -1,19 +1,61 @@
 package org.sopar.presentation.myParkingLot
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import dagger.hilt.android.AndroidEntryPoint
 import org.sopar.R
 import org.sopar.databinding.FragmentMyParkingLotBinding
+import org.sopar.domain.entity.NetworkState
+import org.sopar.presentation.base.BaseErrorDialog
 import org.sopar.presentation.base.BaseFragment
 
+@AndroidEntryPoint
 class MyParkingLot : BaseFragment<FragmentMyParkingLotBinding>(R.layout.fragment_my_parking_lot) {
+    private val myParkingLotViewModel by viewModels<MyParkingLotViewModel>()
+    private val parkingLotAdapter: MyParkingLotAdapter = MyParkingLotAdapter()
+
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentMyParkingLotBinding {
         return FragmentMyParkingLotBinding.inflate(inflater, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setRecyclerViewAdapter()
+        setObserve()
+        myParkingLotViewModel.getParkingLotByUserId()
+    }
+
+    private fun setObserve() {
+        myParkingLotViewModel.parkingLots.observe(viewLifecycleOwner) { parkingLotList ->
+            parkingLotAdapter.submitList(parkingLotList)
+        }
+
+        myParkingLotViewModel.getParkingLotStatus.observe(viewLifecycleOwner) { status ->
+            if (status == NetworkState.FAIL) {
+                val dialog = BaseErrorDialog(R.string.get_parking_lot_error)
+                dialog.show(requireActivity().supportFragmentManager, "getParkingLotErrorDialog")
+            }
+        }
+    }
+
+    private fun setRecyclerViewAdapter() {
+        parkingLotAdapter.setOnItemClickListener { parkingLot ->
+
+        }
+
+        binding.listParkingLot.apply {
+            setHasFixedSize(true)
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            adapter = parkingLotAdapter
+        }
+    }
+
 }
---
+++ app/src/main/java/org/sopar/presentation/myParkingLot/MyParkingLotViewModel.kt
@@ -0,0 +1,40 @@
+package org.sopar.presentation.myParkingLot
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import org.sopar.data.remote.response.ParkingLot
+import org.sopar.domain.entity.NetworkState
+import org.sopar.domain.repository.AuthRepository
+import org.sopar.domain.repository.ParkingLotRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class MyParkingLotViewModel @Inject constructor(
+    private val parkingLotRepository: ParkingLotRepository,
+    private val authRepository: AuthRepository
+): ViewModel() {
+    private val _parkingLots = MutableLiveData(listOf<ParkingLot>())
+    val parkingLots: LiveData<List<ParkingLot>> get() = _parkingLots
+    private val _getParkingLotStatus = MutableLiveData(NetworkState.LOADING)
+    val getParkingLotStatus: LiveData<NetworkState> get() = _getParkingLotStatus
+
+    fun getParkingLotByUserId() {
+        viewModelScope.launch {
+            try {
+                val userId = authRepository.getUId().first()
+                val response = parkingLotRepository.getParkingLotByUser(userId)
+                _parkingLots.postValue(response.body())
+            } catch (e: java.lang.Exception) {
+                Log.d("getParkingLotByUserId", e.toString())
+                _getParkingLotStatus.postValue(NetworkState.FAIL)
+            }
+        }
+
+    }
+}
