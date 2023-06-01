2af11bb62096cc61d406095b4c668f043ed652b2
915dbfl <dbfl00915@naver.com>
2023-06-01 18:25:42 +0900

    [Feat] #82 fcm 토큰 서버에 저장 처리

--- app/src/main/java/org/sopar/presentation/entry/EntryActivity.kt
+++ app/src/main/java/org/sopar/presentation/entry/EntryActivity.kt
@@ -2,21 +2,31 @@ package org.sopar.presentation.entry
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+import dagger.hilt.android.AndroidEntryPoint
+import org.sopar.SoparFirebaseMessagingService
 import org.sopar.databinding.ActivityEntryBinding
 import org.sopar.presentation.main.MainActivity
 import org.sopar.presentation.notice.NoticeActivity
 import org.sopar.presentation.register.RegisterActivity
 
+@AndroidEntryPoint
 class EntryActivity: AppCompatActivity() {
     private val binding: ActivityEntryBinding by lazy {
         ActivityEntryBinding.inflate(layoutInflater)
     }
+    private val entryViewModel by viewModels<EntryViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         init()
+        getDeviceToken()
+
     }
 
     private fun init() {
@@ -45,5 +55,19 @@ class EntryActivity: AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    fun getDeviceToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(
+            OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.d("TokenTest", "Fetching FCM registration token failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                val token = task.result
+                entryViewModel.setUserFCMToken(token)
+            }
+        )
     }
 }
